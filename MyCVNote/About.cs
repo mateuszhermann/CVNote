79d5853d7d4b62818c9e133440e34515d7060aa4
mateuszhermann <majonez15066@wp.pl>
2021-05-27 00:17:06 +0200

    nowy plik:       MyCVNote/About.cs 	nowy plik:       MyCVNote/MainWindow.cs 	nowy plik:       MyCVNote/MyCVNote.csproj 	nowy plik:       MyCVNote/Program.cs 	nowy plik:       MyCVNote/Properties/AssemblyInfo.cs 	nowy plik:       MyCVNote/gtk-gui/MainWindow.cs 	nowy plik:       MyCVNote/gtk-gui/MyCVNote.About.cs 	nowy plik:       MyCVNote/gtk-gui/generated.cs 	nowy plik:       MyCVNote/gtk-gui/gui.stetic 	nowy plik:       MyCVNote/obj/x86/Debug/.NETFramework,Version=v4.7.AssemblyAttributes.cs 	nowy plik:       MyCVNote/obj/x86/Debug/MyCVNote.csproj.CoreCompileInputs.cache 	nowy plik:       MyCVNote/obj/x86/Debug/MyCVNote.csproj.FileListAbsolute.txt 	nowy plik:       MyCVNote/obj/x86/Debug/MyCVNote.csprojAssemblyReference.cache 	nowy plik:       MyCVNote/obj/x86/Debug/MyCVNote.exe 	nowy plik:       MyCVNote/obj/x86/Debug/MyCVNote.pdb 	nowy plik:       MyCVNote/obj/x86/Release/.NETFramework,Version=v4.7.AssemblyAttributes.cs 	nowy plik:       MyCVNote/obj/x86/Release/MyCVNote.csproj.CoreCompileInputs.cache 	nowy plik:       MyCVNote/obj/x86/Release/MyCVNote.csproj.FileListAbsolute.txt 	nowy plik:       MyCVNote/obj/x86/Release/MyCVNote.exe

---
+++ MyCVNote/About.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MyCVNote
+{
+    public partial class About : Gtk.Window
+    {
+        public About() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+    }
+}
---
+++ MyCVNote/MainWindow.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Gtk;
+using System.IO;
+using MyCVNote;
+
+public partial class MainWindow : Gtk.Window
+{
+    string CurrentFile;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void Open(object sender, EventArgs e)
+    {
+        FileChooserDialog filechooser =
+        new FileChooserDialog("Choose the file to open",
+            this,
+            FileChooserAction.Open,
+            "Cancel", ResponseType.Cancel,
+            "Open", ResponseType.Accept);
+
+        if (filechooser.Run() == (int)ResponseType.Accept)
+        {
+            string[] file = File.ReadAllLines(filechooser.Filename);
+            CurrentFile = filechooser.Filename;
+            foreach (var line in file)
+            {
+                textview2.Buffer.Text += line + "\n";
+            }
+
+        }
+
+        filechooser.Destroy();
+
+    }
+
+    protected void SaveAs(object sender, EventArgs e)
+    {
+        FileChooserDialog fileChooser = new FileChooserDialog("Save Dialog",
+        this,
+        FileChooserAction.Save,
+        "Cancel", ResponseType.Cancel,
+        "Save", ResponseType.Accept
+        );
+        if (fileChooser.Run() == (int)ResponseType.Accept)
+        {
+            File.WriteAllText(fileChooser.Filename, textview2.Buffer.Text);
+
+        }
+        fileChooser.Destroy();
+    }
+
+    protected void Save(object sender, EventArgs e)
+    {
+        if (!File.Exists(CurrentFile))
+        {
+            button13.Click();
+        }
+        File.WriteAllText(CurrentFile, textview2.Buffer.Text);
+    }
+
+    protected void AboutWindow(object sender, EventArgs e)
+    {
+        About about = new About();
+
+        about.Show();
+    }
+}
